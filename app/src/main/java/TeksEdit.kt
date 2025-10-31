2daf840aac30e93c22121171744cb94de69db8d7
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-10-31 09:17:36 +0700

    variable untuk menyimpan data yang di peroleh dari komponen UI

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -7,7 +7,14 @@ import androidx.compose.ui.Modifier
 
 fun FormDataDiri(modifier: Modifier
 ){
+    //variable-variable untuk mengingat nilai masukan dari keyboard
     var textNama by remember { mutableStateOf("")}
     var textAlamat by remember { mutableStateOf("")}
     var textJK  by remember { mutableStateOf("")}
+
+    //variable-variable untuk menyimpan data yang di peroleh dari komponen UI
+    var nama by remember { mutableStateOf("")}
+    var alamat by remember { mutableStateOf("")}
+    var jenis by remember { mutableStateOf("")}
+
 }
