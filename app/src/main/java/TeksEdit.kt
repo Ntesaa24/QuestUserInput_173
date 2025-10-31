c42c48f573b7b8896a97383b528351fa9518a357
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-10-31 09:14:16 +0700

    variable" untuk mengingat nilai untuk dimaksukan ke keyboard

---
+++ app/src/main/java/TeksEdit.kt
@@ -0,0 +1,13 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun FormDataDiri(modifier: Modifier
+){
+    var textNama by remember { mutableStateOf("")}
+    var textAlamat by remember { mutableStateOf("")}
+    var textJK  by remember { mutableStateOf("")}
+}
