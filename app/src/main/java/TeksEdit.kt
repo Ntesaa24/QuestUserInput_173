b5f158e8bff39ad24bac9e2742825602388b8476
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-10-31 10:15:16 +0700

    membuat text field almat

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -1,9 +1,17 @@
+
+import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -15,56 +23,72 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.prakpam_userinput.R
 
 @Composable
 
 fun FormDataDiri(modifier: Modifier
-){
+) {
     //variable-variable untuk mengingat nilai masukan dari keyboard
-    var textNama by remember { mutableStateOf("")}
-    var textAlamat by remember { mutableStateOf("")}
-    var textJK  by remember { mutableStateOf("")}
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
     //variable-variable untuk menyimpan data yang di peroleh dari komponen UI
-    var nama by remember { mutableStateOf("")}
-    var alamat by remember { mutableStateOf("")}
-    var jenis by remember { mutableStateOf("")}
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 
-    val gender:List<String> = listOf("Laki-laki, Perempuan")
-    Column(modifier = Modifier.padding(top = 50.dp),
+    val gender: List<String> = listOf("Laki-laki, Perempuan")
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
 
-        horizontalAlignment = Alignment.CenterHorizontally){
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
             value = textNama,
             singleLine = true,
             shape = MaterialTheme.shapes.large,
             modifier = Modifier.width(250.dp),
 
-            label = {Text(text = "Nama Lengkap")},
+            label = { Text(text = "Nama Lengkap") },
             onValueChange = {
                 textNama = it
             }
         )
 
-    Row {
-        gender.forEach { item ->
-            Row ( modifier = Modifier.selectable(
-                selected = textJK == item,
-
-                onClick = { textJK = item }
-            ), verticalAlignment = Alignment.CenterVertically){
-                RadioButton(
+        Row {
+            gender.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
                     selected = textJK == item,
 
-                    onClick = {
-                        textJK = item
-                    })
-                Text(item)
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
 
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
+
+                }
             }
         }
-    }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(250.dp),
 
-}
+            label = { Text(text = "Alamat Lengkap") },
+
+            onValueChange = {
+                textAlamat = it
+            }
+        )
